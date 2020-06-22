8355dc3d2cc58690348240951124a3e660eda2ec
huyennhu99 <vongochuyennhu99@gmail.comgit config --global user.name huyennhu99>
2020-06-22 19:44:13 +0700

    Giao diện

--- routers/_account.router.js
+++ routers/_account.router.js
@@ -4,15 +4,13 @@ const router = express.Router();
 
 router.get('/login', async function (req, res) {
     res.render('vwAccount/index', {
-      layout: false,
-      style :'style.css'
+      layout:false,
+      style: 'main.css'
     });
   })
 router.get('/register',function(req,res)
 {
-  res.render('vwAccount/SignUp',{
-    layout:false
-  })
+  res.render('vwAccount/SignUp')
 })
 
 module.exports = router;
--- routers/news.route.js
+++ routers/news.route.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 router.get('/',function (req, res) {
-    res.render('vwNews/Content');
+    res.render('vwNews/Content',
+    {
+        style:'main.css'
+    });
 });
 module.exports = router;
